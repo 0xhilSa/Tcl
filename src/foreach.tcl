b1e74f50d533e690905968e977893fbb2d0b4b8b
Sahil Rajwar <justsahilrajwar2004@gmail.com>
2025-04-22 00:34:30 +0530

    for each

--- src/foreach.tcl
+++ src/foreach.tcl
@@ -4,3 +4,9 @@ set names {Max Lando Hamilton Leclerc Piastri};
 foreach name $names {
   puts "Hello, $name";
 }
+
+set data {1 2 3 4 5 6};
+foreach x $data {
+  set y [expr $x * 2];
+  puts "$x doubled is $y";
+}
